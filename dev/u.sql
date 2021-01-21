fed029eebb34999382f17c423178af06b5febb4a
Scoindy <swalkinshaw@gmail.com>
2021-01-21 14:17:49 +1300

    Delete u.sql

--- dev/u.sql
+++
@@ -1,7 +0,0 @@
-DROP USER 'scott'@'localhost';
-CREATE USER 'scott'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'scott'@'localhost' WITH GRANT OPTION;
-DROP USER 'scott'@'%';
-CREATE USER 'scott'@'%' IDENTIFIED BY 'scott';
-GRANT ALL PRIVILEGES ON *.* TO 'scott'@'%' WITH GRANT OPTION;
-
